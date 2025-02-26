645aee8413b9ab4d05d8715581584e189a76f2b4
Thiago Carvalho <thiagoomaciel@hotmail.com>
2025-02-25 22:58:30 -0300

    chore: made final configurations

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
+// Contador para identificação dos itens da lista
 let calc = 5
 
+// Acessando elementos da DOM
 const form = document.querySelector("form")
 const input = document.querySelector("input")
 const ul = document.querySelector("ul")
 const trash = document.querySelector(".trash")
 const alertMessage = document.querySelector("#alert")
+const closeAlert = document.querySelector("#alert div button")
 
-// Impede números no input
+// Impede números no input.
 input.addEventListener("input", () => {
     const regex = /\d+/g;
     
@@ -15,7 +18,7 @@ input.addEventListener("input", () => {
     }
 })
 
-// Evento de submit para adicionar novo item
+// Evento de submit para adicionar novo item.
 form.addEventListener("submit", (event) =>{
     event.preventDefault()
 
@@ -25,17 +28,27 @@ form.addEventListener("submit", (event) =>{
     input.value = ""
 })
 
+// Evento para remover o item da lista.
 ul.addEventListener("click", (event) => {
     if (event.target.classList.contains("trash")) {
-        const listItem = event.target.closest("li"); // Seleciona o elemento pai <li>
+        const listItem = event.target.closest("li"); // Seleciona o elemento pai <li>.
         if (listItem) {
             listItem.remove();
             alertMessage.style.display = "block"
+            // Contador para fechar o alerta em 10 segundos.
+            setTimeout(() => {
+                alertMessage.style.display = "none";
+            }, 10000);
         }
     }
 });
 
-// Adiciona um novo item à lista
+// Evento para fechar o alerta de item removido da lista
+closeAlert.addEventListener("click", () =>{
+    alertMessage.style.display = "none"
+})
+
+// Adiciona um novo item à lista.
 function newItem(value){
     try {
         const newItemList = document.createElement("li")
@@ -50,7 +63,7 @@ function newItem(value){
         newDiv.append(newInput)
         newItemList.append(newDiv)
 
-        // Criando o span do texto do item da lista
+        // Criando o span do texto do item da lista.
         const newSpan = document.createElement("span")
         newSpan.textContent = value
         newItemList.append(newSpan)
